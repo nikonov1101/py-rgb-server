e336471f1e898e2899dab050c2ec743af5c0ee5f
Alexander Nikonov <alex@sshaman.ru>
2016-06-29 17:10:55 +0500

    add get-set api controllers, add index page server

---
+++ app/index_page/__init__.py
@@ -0,0 +1,4 @@
+from serialwrapper.wrapper import MSerial
+
+
+serial = MSerial(device='/dev/tty.wchusbserial1410')
--- app/index_page/controllers.py
+++ app/index_page/controllers.py
@@ -1,14 +1,26 @@
-"""
-    Index controllers:
-    1. Render site index page
-"""
-from flask import Blueprint, render_template
+from flask import Blueprint, request, jsonify, render_template
+
+from . import serial
 
 # Define the blueprint: 'index'
 index = Blueprint('index', __name__, url_prefix='/')
 
-# Set the route and accepted methods
-@index.route('/', methods=['GET'])
-def show_index():
-    """ just render index page template """
-    return render_template("index/index.html")
+
+@index.route('/')
+def index_page():
+    return render_template('index/index.html')
+
+
+@index.route('get', )
+def get_color():
+    return jsonify(colors=serial.get_rgb())
+
+
+@index.route('set', methods=['GET', ])
+def set_color():
+    red = request.args.get('r', '0')
+    green = request.args.get('g', '0')
+    blue = request.args.get('b', '0')
+
+    serial.set_rgb(red, green, blue)
+    return jsonify(colors=serial.get_rgb())
